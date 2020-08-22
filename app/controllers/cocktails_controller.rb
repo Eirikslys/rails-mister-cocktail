f8051a0af138bf14a61d6b513d39c9c70bb07eac
eirik lysenstoen <elysenstoen@gmail.com>
2020-08-22 16:29:58 +0200

    added doses controller, got the form and everything else for the cocktail views working.

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -14,7 +14,7 @@ class CocktailsController < ApplicationController
   def create
     @cocktail = Cocktail.new(cocktail_params)
     if @cocktail.save
-      redirect_to path_cocktail(@cocktail)
+      redirect_to cocktail_path(@cocktail)
     else
       render :new
     end
