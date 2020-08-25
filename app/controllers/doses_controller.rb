c4554d437da8d4b023f0c2fcd62021fce2b9ac8c
eirik lysenstoen <elysenstoen@gmail.com>
2020-08-25 20:28:43 +0200

    got the doses controller working minus delete

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,16 +1,32 @@
 class DosesController < ApplicationController
 
-  def index
-    @doses = Dose.all
-  end
-
-  def show
-  end
-
   def new
+    find_cocktail
+    # @cocktail.doses = Dose.new
+    @dose = Dose.new
+    @dose.cocktail = @cocktail
   end
 
   def create
+    find_cocktail
+    @dose = Dose.new(dose_params)
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
   end
 
+  def destroy
+  end
+
+  private
+  def find_cocktail
+    @cocktail = Cocktail.find(params[:cocktail_id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
 end
